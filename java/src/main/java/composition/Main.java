d154a6a10a4774c0719555e7b4e4c888b1855345
johan.martinsson <johan.martinsson@dexyos.fr>
2021-12-21 16:13:53 +0100

    clean up

--- java/src/main/java/composition/Main.java
+++ java/src/main/java/composition/Main.java
@@ -3,15 +3,20 @@ package composition;
 public class Main {
 
     public static void main(String[] args) {
-        Menu menu = new Menu(new Brownie(), new QuicheLorraine());
+        Brownie brownie = new Brownie();
+        QuicheLorraine quicheLorraine = new QuicheLorraine();
+        CremeCaramel cremeCaramel = new CremeCaramel();
+        FiletMignon filetMignon = new FiletMignon();
+
+        Menu menu = new Menu(brownie, quicheLorraine);
         printMenu(menu);
-        Menu meatLoversMenu = new Menu(new Brownie(), new FiletMignon());
+        Menu meatLoversMenu = new Menu(brownie, filetMignon);
         printMenu(meatLoversMenu);
-        Menu gourmetMenu = new Menu(new CremeCaramel(), new QuicheLorraine());
+        Menu gourmetMenu = new Menu(cremeCaramel, quicheLorraine);
         printMenu(gourmetMenu);
 
         // new feature!!!
-        Menu noGlutenMenu = new Menu(new CremeCaramel(), new FiletMignon());
+        Menu noGlutenMenu = new Menu(cremeCaramel, filetMignon);
         printMenu(noGlutenMenu);
     }
 
