0e8857473bd2cbcea4f66d1b544f08fc0b08bd04
Johan Martinsson <jma@notsodark.com>
2023-03-24 23:17:58 +0100

    move code to prod

---
+++ typescript/src/greeting.ts
@@ -0,0 +1,38 @@
+export function greet(...names: string[]) {
+  names = normalizeNames(names);
+
+  const shoutingNames = formatNames(names.filter((n) => n.toUpperCase() === n))
+  const normalNames = formatNames(names.filter((n) => n.toUpperCase() !== n))
+  if (normalNames && shoutingNames) {
+    return `hello, ${normalNames}` + ". AND " + `HELLO ${shoutingNames}!`;
+  }
+  if (normalNames) {
+    return `hello, ${normalNames}`;
+  }
+  return `HELLO ${shoutingNames}!`
+}
+
+function normalizeNames(names: string[]) {
+  if (names.length === 1 && !names[0]) {
+    names = ["my friend"]
+  }
+
+  const allNames = names
+    .map((name) => name.replace(/"/g, ""))
+    .map((value) => value.split(", "));
+
+  return flatten(allNames)
+}
+
+function flatten(names: string[][]) {
+  return names.reduce((acc, value) => acc.concat(...value), []);
+}
+
+function formatNames(names: string[]) {
+  if (names.length <= 1) {
+    return names[0]
+  } else {
+    const last = names.splice(names.length - 1, 1);
+    return names.join(', ') + " and " + last
+  }
+}
--- typescript/test/greeting.spec.ts
+++ typescript/test/greeting.spec.ts
@@ -1,45 +1,6 @@
 // tslint:disable-next-line:no-implicit-dependencies
-import {assert, expect} from 'chai';
-
-function greet(...names: string[]) {
-  names = normalizeNames(names);
-
-  const shoutingNames = formatNames(names.filter((n) => n.toUpperCase() === n))
-  const normalNames = formatNames(names.filter((n) => n.toUpperCase() !== n))
-  if (normalNames && shoutingNames) {
-    return `hello, ${normalNames}` + ". AND " + `HELLO ${shoutingNames}!`;
-  }
-  if (normalNames) {
-    return `hello, ${normalNames}`;
-  }
-  return `HELLO ${shoutingNames}!`
-}
-
-function normalizeNames(names: string[]) {
-  if (names.length === 1 && !names[0]) {
-    names = ["my friend"]
-  }
-
-  const allNames = names
-    .map((name) => name.replace(/"/g, ""))
-    .map((value) => value.split(", "));
-
-  return flatten(allNames)
-}
-
-function flatten(names: string[][]) {
-  return names.reduce((acc, value) => acc.concat(...value), []);
-}
-
-function formatNames(names: string[]) {
-  if (names.length <= 1) {
-    return names[0]
-  } else {
-    const last = names.splice(names.length - 1, 1);
-    return names.join(', ') + " and " + last
-  }
-}
-
+import {expect} from 'chai';
+import {greet} from "../src/greeting";
 
 describe('greeting', () => {
   [
