c748c3a49cdad7256b77b62d1fa232abf1e02c12
Johan Martinsson <jma@notsodark.com>
2023-03-24 21:34:11 +0100

    refactor tests

--- typescript/test/kata.spec.ts
+++ typescript/test/kata.spec.ts
@@ -1,6 +1,5 @@
-
 // tslint:disable-next-line:no-implicit-dependencies
-import { assert, expect } from 'chai';
+import {assert, expect} from 'chai';
 
 function greet(...names: string[]) {
 
@@ -21,18 +20,31 @@ function greet(...names: string[]) {
 }
 
 describe('greeting', () => {
-  it('greet', () => {
-      expect(greet("joe")).equal('hello, joe');
-      expect(greet(undefined)).equal('hello, my friend');
-      expect(greet('JERRY')).equal('HELLO JERRY!');
-      expect(greet('mary', 'jane')).equal('hello, mary and jane')
-      expect(greet('mary', 'jane', 'charlotte')).equal('hello, mary, jane and charlotte')
+  [
+    ['joe', 'hello, joe'],
+    [undefined, 'hello, my friend'],
+    ['JERRY', 'HELLO JERRY!']
+  ].forEach(([name, expected]) => {
+    it('should ', () => {
+      expect(greet(name)).equal(expected);
+    });
   });
+  [
+    [['mary', 'jane'], 'hello, mary and jane'],
+    [['mary', 'jane', 'charlotte'], 'hello, mary, jane and charlotte'],
+  ].forEach(([names, expected]) => {
+    it('should ', () => {
+      expect(greet(...names)).equal(expected);
+    });
+  })
 
-
-
-
-
+  it('greet', () => {
+    expect(greet("joe")).equal('hello, joe');
+    expect(greet(undefined)).equal('hello, my friend');
+    expect(greet('JERRY')).equal('HELLO JERRY!');
+    expect(greet('mary', 'jane')).equal('hello, mary and jane')
+    expect(greet('mary', 'jane', 'charlotte')).equal('hello, mary, jane and charlotte')
+  });
 
 
 });
